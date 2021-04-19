bd22df1c249d20a4ff073211cc04f86b9a0dd2d2
Randy <randy@clarkehill.com>
2021-04-18 18:20:36 -0700

    TextField has option to turn off auto correct

--- Widgets/TextFieldActive.swift
+++ Widgets/TextFieldActive.swift
@@ -14,6 +14,7 @@ import Introspect
 struct TextFieldActive: View {
     var title: String?
     var placeholder: String
+    var disableAutocorrection: Bool = true
     @Binding var text: String
 
     var body: some View {
@@ -30,6 +31,7 @@ struct TextFieldActive: View {
                     textField.addDoneButton()
                 }
                 .modifier(TextFieldClearButton(text: $text))
+                .disableAutocorrection(disableAutocorrection)
         }
     }
 }
